6f97f74a818336c664bf3de787c91fd6957fbec1
Sashank <sashank.pindiproli@appdirect.com>
2021-10-06 21:23:18 +0530

    Finishing touches for songs app

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import likeLyric from '../queries/likeLyric';
+import mutation from '../queries/likeLyric';
 
 class LyricList extends Component
 {
-    onLike (lyricId)
+    onLike (id,likes)
     { 
-        
+        this.props.mutate( {
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                LikeLyric: {
+                    id: id,
+                    __typename: 'LyricType',
+                    likes: likes + 1 
+                }
+            }
+        })
     }
     renderLyrics () {
-        return this.props.lyrics.map( ( { id, content } ) =>
+        return this.props.lyrics.map( ( { id, content, likes } ) =>
             <li className="collection-item" key={ id }>
                 { content }
-                <i className="material-icons" onClick={ () => this.onLike(id) }>thumb_up</i>
+                <div className="likes-container">
+                    <i className="material-icons" onClick={ () => this.onLike( id,likes ) }>thumb_up</i>{ likes }
+                </div> 
             </li> )
     }
     render ()
@@ -19,4 +34,4 @@ class LyricList extends Component
     }
 }
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
